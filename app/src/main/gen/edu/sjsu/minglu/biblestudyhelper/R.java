bdf190e0fd03ce75529673d516b982a415020c6b
han chen <hc9999dragon@gmail.com>
2017-05-10 14:16:41 -0700

    update two fragments

---
+++ app/src/main/gen/edu/sjsu/minglu/biblestudyhelper/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.sjsu.minglu.biblestudyhelper;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
--- app/src/main/java/edu/sjsu/minglu/biblestudyhelper/MainActivity.java
+++ app/src/main/java/edu/sjsu/minglu/biblestudyhelper/MainActivity.java
@@ -2,18 +2,41 @@ package edu.sjsu.minglu.biblestudyhelper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.widget.ImageView;
+
 
 
 public class MainActivity extends AppCompatActivity {
     private ImageView image_view;
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        image_view = (ImageView) findViewById(R.id.chapter1);
+//        image_view.setImageResource(R.drawable.c2);
+//        image_view.bringToFront();
+//    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image_view = (ImageView) findViewById(R.id.chapter1);
-        image_view.setImageResource(R.drawable.c2);
-        image_view.bringToFront();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragment_top, new topFragment());
+        transaction.replace(R.id.fragment_bot, new botFragment());
+        transaction.commit();
+    }
+
+    public void onbotFragmentInteraction(String string) {
+        // Do stuff
+    }
+
+
+    public void ontopFragmentInteraction(String string) {
+        // Do different stuff
     }
 }
